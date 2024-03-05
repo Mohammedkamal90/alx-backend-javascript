851b006afde6e8af24496c6f1ab6a07e2384fc0e
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-03-05 13:12:16 -0800

    up

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -4,7 +4,7 @@ function getResponseFromAPI() {
   const promise = new Promise((resolve) => {
     // Simulating an API call or any asynchronous operation
     setTimeout(() => {
-      const responseData = { data: "API response data" };
+      const responseData = { data: 'API response data' };
       resolve(responseData);
     }, 1000); // Simulating a delay of 1 second
   });
