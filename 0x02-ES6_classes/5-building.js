346d9a859d9b8697f93f28a7e2ae01ecc66de367
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-03-07 16:44:01 -0800

    task7

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -8,7 +8,6 @@ export default class Building {
   get sqft() {
     return this._sqft;
   }
-
   // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,38 @@
+// Filename: 7-airport.js
+
+const _name = ('_name');
+const _code = ('_code');
+
+export default class Airport {
+  constructor(name, code) {
+    this[_name] = name;
+    this[_code] = code;
+  }
+
+  get name() {
+    return this[_name];
+  }
+
+  set name(value) {
+    this[_name] = value;
+  }
+
+  get code() {
+    return this[_code];
+  }
+
+  set code(value) {
+    this[_code] = value;
+  }
+
+  toString() {
+    return `Airport [${this.code}] { _name: '${this.name}', _code: '${this.code}' }`;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') {
+      return `[object ${this.code}]`;
+    }
+    return this;
+  }
+}
