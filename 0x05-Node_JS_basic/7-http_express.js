37d3b6839846299a7ca2e510839c2bae329e1aea
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-04-29 20:30:27 +0000

    task7

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,33 @@
+// 7-http_express.js
+
+const express = require('express');
+const fs = require('fs').promises;
+const countStudents = require('./3-read_file_async');
+
+// Create an instance of Express
+const app = express();
+
+// Define a route for the root endpoint '/'
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!\n');
+});
+
+// Define a route for the '/students' endpoint
+app.get('/students', async (req, res) => {
+  try {
+    const data = await fs.readFile(process.argv[2], 'utf8');
+    res.write('This is the list of our students\n');
+    await countStudents(process.argv[2]);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Start the Express server
+const PORT = 1245;
+app.listen(PORT, () => {
+  console.log(`Server is running and listening on port ${PORT}`);
+});
+
+// Export the Express app
+module.exports = app;
