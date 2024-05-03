6b669f4867e32e4d366874118def950d40c6c34f
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-05-03 16:52:06 +0000

    upd7

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,34 +1,16 @@
-// 6-http_express.js
-
 const express = require('express');
 
-// Create an instance of Express
 const app = express();
+const port = 1245;
 
-// Define a route for the root endpoint '/'
+// Endpoint for root path
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
+  res.send('Hello Holberton School!');
 });
 
-// Define a route for handling other endpoints
-app.use((req, res) => {
-  res.status(404).send(`<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Error</title>
-</head>
-<body>
-<pre>Cannot GET ${req.url}</pre>
-</body>
-</html>`);
+// Start listening on the specified port
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
 
-// Start the Express server
-const PORT = 1245;
-app.listen(PORT, () => {
-  console.log(`Server is running and listening on port ${PORT}`);
-});
-
-// Export the Express app
 module.exports = app;
