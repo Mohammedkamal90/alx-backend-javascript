088323fdcf1ed10d82dbc5ac9682a9e480db25da
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-05-03 16:56:38 +0000

    task7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,33 +1,29 @@
-// 7-http_express.js
-
 const express = require('express');
-const fs = require('fs').promises;
-const countStudents = require('./3-read_file_async');
+const { countStudents } = require('./3-read_file_async');
 
-// Create an instance of Express
 const app = express();
+const port = 1245;
 
-// Define a route for the root endpoint '/'
+// Endpoint for root path
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
+  res.send('Hello Holberton School!');
 });
 
-// Define a route for the '/students' endpoint
-app.get('/students', async (req, res) => {
-  try {
-    await fs.readFile(process.argv[2], 'utf8');
-    res.write('This is the list of our students\n');
-    await countStudents(process.argv[2]);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+// Endpoint for /students path
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch((error) => { // Using the error variable
+      console.error(error); // Log the error for debugging
+      res.status(404).send('Cannot load the database');
+    });
 });
 
-// Start the Express server
-const PORT = 1245;
-app.listen(PORT, () => {
-  console.log(`Server is running and listening on port ${PORT}`);
+// Start listening on the specified port
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
 
-// Export the Express app
 module.exports = app;
