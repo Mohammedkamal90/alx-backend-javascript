04e34c661d69d0f617a0b3f44245a05ce4eeb715
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-05-03 22:34:06 +0000

    upd

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,29 +2,24 @@ const request = require('request');
 const { expect } = require('chai');
 
 describe('Index page', () => {
-  let server;
-
-  before(() => {
-    server = require('./api');
-  });
-
-  after(() => {
-    server.close();
-  });
-
-  it('should return status code 200', (done) => {
+  it('Correct status code?', (done) => {
     request.get('http://localhost:7865', (error, response) => {
+      if (error) return done(error);
       expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
-  it('should return correct result', (done) => {
+  it('Correct result?', (done) => {
     request.get('http://localhost:7865', (error, response, body) => {
+      if (error) return done(error);
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
 
-  // Add other tests as needed
+  it('Other?', (done) => {
+    // Add any additional tests if needed
+    done();
+  });
 });
