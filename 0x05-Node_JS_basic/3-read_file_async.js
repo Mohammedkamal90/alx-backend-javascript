fc68d11abd6d188df1eb4f81713d6bf9c6b23a11
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-05-03 13:28:10 +0000

    task3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,42 +1,41 @@
-// 3-read_file_async.js
-
 const fs = require('fs');
 
-/**
- * Counts the number of students in each field from the given CSV file asynchronously.
- * @param {string} path - The path to the CSV file.
- * @returns {Promise} A Promise that resolves when the operation is completed successfully.
- */
+function getStudentsList(lines, field) {
+  const list = [];
+  lines.forEach((line) => {
+    const fields = line.split(',');
+    if (fields[3].trim() === field && fields[0].trim() !== '') {
+      list.push(fields[0].trim());
+    }
+  });
+  return list.join(', ');
+}
+
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (error, data) => {
-      if (error) {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
         reject(new Error('Cannot load the database'));
       } else {
-        const lines = data.split('\n').filter((line) => line.trim() !== '');
-        const fields = lines.shift().split(',').filter((field) => field !== 'name');
-        const fieldCount = {};
-        const fieldList = {};
-
-        fields.forEach((field) => {
-          fieldCount[field] = 0;
-          fieldList[field] = [];
-        });
+        const lines = data.trim().split('\n');
+        const fieldCounts = {};
 
         lines.forEach((line) => {
-          const values = line.split(',');
-          fields.forEach((field, index) => {
-            if (values[index] === '1') {
-              fieldCount[field] += 1;
-              fieldList[field].push(values[0]);
-            }
-          });
+          const fields = line.split(',');
+          if (fields.length === 4 && fields[0] !== '') {
+            const field = fields[3].trim();
+            fieldCounts[field] = (fieldCounts[field] || 0) + 1;
+          }
         });
 
-        console.log(`Number of students: ${lines.length}`);
-        fields.forEach((field) => {
-          console.log(`Number of students in ${field}: ${fieldCount[field]}. List: ${fieldList[field].join(', ')}`);
-        });
+        const totalStudents = lines.length - 1;
+        console.log(`Number of students: ${totalStudents}`);
+
+        for (const field in fieldCounts) {
+          if (Object.prototype.hasOwnProperty.call(fieldCounts, field)) {
+            console.log(`Number of students in ${field}: ${fieldCounts[field]}. List: ${getStudentsList(lines, field)}`);
+          }
+        }
 
         resolve();
       }
