60b516456b1c6e0730405014d04078e2f6c443b6
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-05-03 13:14:37 +0000

    upd

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,22 +1,14 @@
 // 1-stdin.js
 
-// Display initial message
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Read user input
-process.stdin.once('data', (data) => {
-  const input = data.toString().trim();
-
-  // Display user's name
-  if (input.length > 0) {
-    process.stdout.write(`Your name is: ${input}\n`);
-  }
-
-  // End the process after handling input
-  process.stdin.destroy();
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}`);
+  process.exit();
 });
 
-// Display closing message after input event handler has finished
-process.stdin.on("close", () => {
-  process.stdout.write('This important software is now closing\n');
-});
+if (!process.stdin.isTTY) {
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
